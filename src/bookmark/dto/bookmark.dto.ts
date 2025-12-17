fb66b377b15963ef026ae63c6a53a21fd4b778a4
Abduselam <abduselamjibril78@gmail.com>
2025-12-17 09:21:17 +0300

    add ApiProperty decorators to AddBookmarkDto and RemoveBookmarkDto for improved Swagger documentation

--- src/bookmark/dto/bookmark.dto.ts
+++ src/bookmark/dto/bookmark.dto.ts
@@ -1,19 +1,38 @@
 import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import type { BookmarkType } from '../entities/bookmark.entity';
 
 export class AddBookmarkDto {
+  @ApiProperty({
+    enum: ['content', 'reciter', 'tafsir'],
+    example: 'content',
+    description: 'Type of the item to bookmark',
+  })
   @IsEnum(['content', 'reciter', 'tafsir'])
   type: BookmarkType;
 
+  @ApiProperty({
+    example: '123',
+    description: 'ID of the item to bookmark',
+  })
   @IsString()
   @IsNotEmpty()
   itemId: string;
 }
 
 export class RemoveBookmarkDto {
+  @ApiProperty({
+    enum: ['content', 'reciter', 'tafsir'],
+    example: 'content',
+    description: 'Type of the item to remove from bookmarks',
+  })
   @IsEnum(['content', 'reciter', 'tafsir'])
   type: BookmarkType;
 
+  @ApiProperty({
+    example: '123',
+    description: 'ID of the item to remove from bookmarks',
+  })
   @IsString()
   @IsNotEmpty()
   itemId: string;
